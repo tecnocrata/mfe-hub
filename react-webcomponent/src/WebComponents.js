e58089fabd477dcd1d0dcfdb6d90d5992abce51f
enrique.ortuno <enrique@ortuno.net>
2021-08-12 17:18:13 -0400

    initial webcomponent structure added

--- react-webcomponent/src/WebComponents.js
+++ react-webcomponent/src/WebComponents.js
@@ -1,5 +1,6 @@
 import defineElement from "./custom-element";
 import { ContactList } from "./contact-list/ContactList";
+import singleSpaHtml from "single-spa-html";
 
 defineElement(
   ContactList,
@@ -7,3 +8,11 @@ defineElement(
   ["headerText", "contacts"],
   ["onContactSelected"]
 );
+
+const htmlLifecycles = singleSpaHtml({
+  template: "<react-contact-list></react-contact-list>",
+});
+
+export const bootstrap = htmlLifecycles.bootstrap;
+export const mount = htmlLifecycles.mount;
+export const unmount = htmlLifecycles.unmount;
--- shell-app/src/tecnox-root-config.ts
+++ shell-app/src/tecnox-root-config.ts
@@ -5,6 +5,12 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
+// import singleSpaHtml from "single-spa-html";
+
+// const webComponentApp = singleSpaHtml({
+//   template: (props) =>
+//     `<react-contact-list attr="${props.attr}"></react-contact-list>`,
+// });
 
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
@@ -23,6 +29,17 @@ registerApplication({
   // @ts-ignore
   app: () => import("vueapp/body"),
   activeWhen: ["/vuejs-module-federation"],
+});
+// registerApplication({
+//   name: "name",
+//   app: webComponentApp,
+//   activeWhen: ["/react-webcomponent"],
+//   // activeWhen: () => true
+// });
+registerApplication({
+  name: "@example/cookie-consent",
+  app: () => System.import("@example/cookie-consent"),
+  activeWhen: ["/react-webcomponent"],
 });
 layoutEngine.activate();
 start();
