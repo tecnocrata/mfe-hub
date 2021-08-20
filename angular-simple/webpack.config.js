60772320813b446d4b511202813faf00f374f126
enrique.ortuno <enrique@ortuno.net>
2021-08-20 16:10:59 -0400

    small adjusments

--- angular-simple/webpack.config.js
+++ angular-simple/webpack.config.js
@@ -6,6 +6,7 @@ const ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin")
 module.exports = {
   entry: {
     // 'root-application': 'src/root-application/root-application.js',
+    index: "src/app2.js",
     "common-dependencies": [
       // We want just one version of angular, so we put it into the common dependencies
       "core-js/client/shim.min.js",
@@ -45,12 +46,13 @@ module.exports = {
     new CleanWebpackPlugin(["dist"]),
     new ContextReplacementPlugin(
       /(.+)?angular(\\|\/)core(.+)?/,
-      path.resolve(__dirname, "../src")
+      path.resolve(__dirname, "../")
     ),
   ],
   devtool: "source-map",
   externals: [],
   devServer: {
     historyApiFallback: true,
+    port: 5002,
   },
 };
