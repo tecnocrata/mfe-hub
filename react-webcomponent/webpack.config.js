063554332d9d4a805c668e0f5c9ad4c9ad286368
enrique.ortuno <enrique@ortuno.net>
2021-08-20 12:34:01 -0400

    react webcomponent app replaced with one simplest one

---
+++ react-webcomponent/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const packageJson = require("./package.json");
+
+module.exports = (opts) => {
+  let isProduction =
+    opts.argv && (opts.argv.p || opts.argv.mode === "production");
+
+  return {
+    mode: isProduction ? "production" : "development",
+    output: {
+      filename: `./src/index.js`,
+      libraryTarget: "system",
+      path: path.resolve(process.cwd(), "dist"),
+      uniqueName: packageJson.name,
+      publicPath: "",
+    },
+    module: {
+      rules: [
+        {
+          parser: {
+            system: false,
+          },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        },
+        {
+          test: /\.html$/i,
+          use: ["html-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            {
+              loader: "postcss-loader",
+            },
+          ],
+        },
+      ],
+    },
+    devtool: "source-map",
+    devServer: {
+      compress: true,
+      port: 5001,
+      historyApiFallback: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      //   firewall: false,
+      //   client: {
+      //     host: "localhost",
+      //   },
+      onListening: ({ compiler }) => {
+        const { https, client } = compiler.options.devServer;
+        const { publicPath, filename } = compiler.options.output;
+        const protocol = https ? "https://" : "http://";
+        // const port = !!client.port ? `:${client.port}` : "";
+        // const path = ["", "auto"].includes(publicPath) ? "/" : publicPath;
+        // console.log(
+        //   `⚡️ single-spa component entry: ${protocol}${client.host}${port}${path}${filename}`
+        // );
+      },
+    },
+    externals: ["single-spa"],
+  };
+};
