369ba6058f64e523a13c158634a763f71e257f7b
enrique.ortuno <enrique@ortuno.net>
2021-08-23 09:32:16 -0400

    bash script added

---
+++ single-spa/start.sh
@@ -0,0 +1,6 @@
+cd shell-app && yarn start; 
+cd angular-application && yarn run serve:single-spa;
+cd react-webcomponent && yarn run dev
+cd vuejs-module-federation && yarn start
+cd react-application && yarn start
+cd angular11-module-federation && npx ng serve
