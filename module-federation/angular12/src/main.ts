09b52bae12c1c785d75933d5d10efb1556db30a6
enrique.ortuno <enrique@ortuno.net>
2021-08-23 15:00:55 -0400

    angular12: module-federation config added

--- module-federation/angular12/src/main.ts
+++ module-federation/angular12/src/main.ts
@@ -1,12 +1,2 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+console.log('Angular12 app');
+import('./bootstrap').catch((err) => console.error(err));
