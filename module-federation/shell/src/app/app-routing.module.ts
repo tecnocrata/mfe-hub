40ed48e8ef88e8fd326c72bb19bb6e8407635ced
enrique.ortuno <enrique@ortuno.net>
2021-08-24 10:29:43 -0400

    angular12: mfe showed but not correctly

--- module-federation/shell/src/app/app-routing.module.ts
+++ module-federation/shell/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [{ path: '', component: WelcomeComponent }];
+const routes: Routes = [
+  {
+    path: 'angular-12',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:6001/remoteEntry.js',
+        remoteName: 'angular12',
+        exposedModule: './EditorsModule',
+      }).then((m) => m.EditorsModule),
+  },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
