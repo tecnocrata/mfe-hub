de359d7a619fdef028288dacc29a288fa88178ac
enrique.ortuno <enrique@ortuno.net>
2021-09-06 15:47:53 -0400

    mysql support added but commented

--- versioning/code/mix.exs
+++ versioning/code/mix.exs
@@ -37,6 +37,7 @@ defmodule App.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
+      # {:myxql, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
