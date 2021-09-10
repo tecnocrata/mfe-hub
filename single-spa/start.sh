ea304b707c5b2328e8bbcb3835b73b8b45cdeaff
enrique.ortuno <enrique@ortuno.net>
2021-09-10 18:05:33 -0400

    single-spa package.json created

--- single-spa/start.sh
+++
@@ -1,6 +0,0 @@
-cd shell-app && yarn start; 
-cd angular-application && yarn run serve:single-spa;
-cd react-webcomponent && yarn run dev
-cd vuejs-module-federation && yarn start
-cd react-application && yarn start
-cd angular11-module-federation && npx ng serve
